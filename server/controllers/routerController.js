9064dac443e4df25e83d0bf8cf010445bf0f0506
yakov cohen <yakovc1431@gmail.com>
2021-11-30 19:30:51 +0200

    add login func

--- server/controllers/routerController.js
+++ server/controllers/routerController.js
@@ -1,10 +1,16 @@
 //
 const USERS = [
   {
+    email: 'admin@email.com',
+    name: 'admin',
+    password: 'Rc123456!',
+    isAdmin: true,
+  },
+  {
     email: 'yakov@gmail.com',
     name: 'yakov',
     password: '123',
-    isAdmin: true,
+    isAdmin: false,
   },
   {
     email: 'ori@gmail.com',
@@ -13,7 +19,7 @@ const USERS = [
     isAdmin: false,
   },
 ];
-const INFORMATION = []; // {email: ${email}, info: "${name} info"}
+const INFORMATION = [{ email: 'yakov2@gmail.com', info: 'yakov info' }]; // {email: ${email}, info: "${name} info"}
 const REFRESHTOKENS = [];
 
 exports.register = (req, res, next) => {
@@ -30,8 +36,33 @@ exports.register = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-  res.send('accessToken, refreshToken , email, name, isAdmin');
-  //   res.send('cannot find user');
+  const user = req.body;
+  // console.log(user);
+  const body = {};
+  let flag = 0;
+  for (let user1 of USERS) {
+    if (user.email === user1.email && user.password === user1.password) {
+      console.log('yes');
+      body.email = user1.email;
+      body.name = user1.name;
+      body.isAdmin = user1.isAdmin;
+      const accessToken = jwt.sign(user, ACCESS_TOKEN, {
+        expiresIn: '60s',
+      });
+      body.accessToken = accessToken;
+      const refreshToken = jwt.sign(user, REFRESH_TOKENS);
+      body.refreshToken = refreshToken;
+      REFRESHTOKENS.push(refreshToken);
+      return res.status(200).send(body);
+    } else if (user.email === user1.email || user.password === user1.password) {
+      flag = 1;
+    }
+  }
+  if (flag === 0) {
+    return res.status(404).send('cannot find user');
+  } else if (flag === 1) {
+    return res.status(403).send('User or Password incorrect');
+  }
 };
 
 exports.tokenValidate = (req, res, next) => {
