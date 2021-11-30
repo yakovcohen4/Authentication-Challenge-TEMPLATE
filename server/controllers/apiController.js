c6e5bf502c66170bc2c359e58622b00b9fb77433
yakov cohen <yakovc1431@gmail.com>
2021-11-30 19:32:35 +0200

    add information func

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -18,8 +18,27 @@ const REFRESHTOKENS = [];
 //
 
 exports.information = (req, res, next) => {
-  res.send('information');
-  //   res.send('cannot find user');
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token === null) {
+    return res.status(401).send('Access Token Required');
+  }
+
+  jwt.verify(token, ACCESS_TOKEN, (err, user) => {
+    if (err) {
+      return res.status(403).send('Invalid Access Token');
+    }
+    const body = {};
+    for (let info of INFORMATION) {
+      if (info.email === user.email) {
+        body.email = user.email;
+        body.info = info.info;
+      }
+    }
+    res.status(200).send(body);
+    next();
+  });
 };
 
 exports.users = (req, res, next) => {
