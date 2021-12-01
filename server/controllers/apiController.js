63cb2004ee60396196e0613eae66fe6c79be21d5
yakov cohen <yakovc1431@gmail.com>
2021-12-01 16:06:39 +0200

    add users func

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,21 +1,9 @@
 //
-const USERS = [
-  {
-    email: 'yakov@gmail.com',
-    name: 'yakov',
-    password: '123',
-    isAdmin: true,
-  },
-  {
-    email: 'ori@gmail.com',
-    name: 'ori',
-    password: '123456',
-    isAdmin: false,
-  },
-];
-const INFORMATION = []; // {email: ${email}, info: "${name} info"}
-const REFRESHTOKENS = [];
-//
+const jwt = require('jsonwebtoken');
+
+// IMPORT TOKENS AND DATA
+const { ACCESS_TOKEN_SECRET, REFRESH_ACCESS_TOKEN_SECRET } = require('./env');
+const { USERS, INFORMATION, REFRESHTOKENS } = require('../helpers/data');
 
 exports.information = (req, res, next) => {
   const authHeader = req.headers['authorization'];
@@ -25,7 +13,7 @@ exports.information = (req, res, next) => {
     return res.status(401).send('Access Token Required');
   }
 
-  jwt.verify(token, ACCESS_TOKEN, (err, user) => {
+  jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) {
       return res.status(403).send('Invalid Access Token');
     }
@@ -36,12 +24,29 @@ exports.information = (req, res, next) => {
         body.info = info.info;
       }
     }
-    res.status(200).send(body);
+    res.status(200).send([body]);
     next();
   });
 };
 
 exports.users = (req, res, next) => {
-  res.send('users');
-  //   res.send('cannot find user');
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) {
+    return res.status(401).send('Access Token Required');
+  }
+  jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).send('Invalid Access Token');
+    }
+    for (let user1 of USERS) {
+      if (user1.email === user.email) {
+        if (user1.isAdmin) {
+          return res.status(200).send(USERS);
+        }
+      }
+    }
+    return res.status(403).send('Invalid Access Token');
+  });
+  next();
 };
