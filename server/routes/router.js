de76a3256f6def8c04a637fbe254353ab51433b4
yakov cohen <yakovc1431@gmail.com>
2021-12-01 16:20:12 +0200

    fix users router

--- server/routes/router.js
+++ server/routes/router.js
@@ -13,10 +13,10 @@ router.get('/', (req, res) => {
   res.send('in');
 });
 
-router.post('/users/register', register);
-router.post('/users/login', login);
-router.post('/users/tokenValidate', tokenValidate);
-router.post('/users/token', token);
-router.post('/users/logout', logout);
+router.post('/register', register);
+router.post('/login', login);
+router.post('/tokenValidate', tokenValidate);
+router.post('/token', token);
+router.post('/logout', logout);
 
 module.exports = router;
